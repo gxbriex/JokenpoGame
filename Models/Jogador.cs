6b144efdcadcc3197ac840d8265f11b1c3618191
Gabriel Rodrigues <gaberodrigues2001@gmail.com>
2025-09-04 19:17:14 -0300

    Cria a pasta Models e adiciona os arquivos de modelo

---
+++ Models/Jogador.cs
@@ -0,0 +1,24 @@
+﻿namespace JokenpoGame.Models
+{
+    public class Jogador
+    {
+        public string Nome { get; set; }
+        public int Pontuacao { get; set; }
+
+        public Jogador(string nome)
+        {
+            Nome = nome;
+            Pontuacao = 0; // Todo jogador começa com 0 pontos
+        }
+
+        public void AdicionarPonto()
+        {
+            Pontuacao++;
+        }
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Jogador: {Nome} | Pontos: {Pontuacao}");
+        }
+    }
+}
