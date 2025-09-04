7082d9d21d587368f6281273331d5b5b8c789d4e
Gabriel Rodrigues <gaberodrigues2001@gmail.com>
2025-09-04 19:17:29 -0300

    Adiciona a lógica principal do jogo em Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using JokenpoGame.Services;
+
+namespace JokenpoGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nomeJogador = Console.ReadLine();
+            
+            if (string.IsNullOrWhiteSpace(nomeJogador))
+            {
+                nomeJogador = "Jogador";
+            }
+            
+            JokenpoGameService jogo = new JokenpoGameService(nomeJogador);
+            
+            jogo.IniciarJogo();
+        }
+    }
+}
